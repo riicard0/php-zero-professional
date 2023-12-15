a6b930f68b74a34d0b57e8a1d1e84104bc5dc79b
Ricardo Martins <riicardo.marttyns@gmail.com>
2023-12-15 06:55:03 -0300

    :up: Project: Portal Website 1.6 (menu mobile)

--- projects/portal/assets/js/script.js
+++ projects/portal/assets/js/script.js
@@ -28,17 +28,13 @@ function mudarSlide(pos) {
   document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-" + (slidewidth * slideItem) + "px";
 }
 
-/*
-
 //MENU
 function toggleMenu() {
   var menu = document.getElementById("menu");
 
-  if (menu.style.display == 'none') || menu.style.display = '') {
+  if (menu.style.display == 'none' || menu.style.display == '') {
     menu.style.display = "block";
   } else {
     menu.style.display = "none";
   }
 }
-
-*/
