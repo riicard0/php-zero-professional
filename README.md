081116e729f6155bfd7cfaa33688a22cbd22b6c8
Ricardo Martins <riicardo.marttyns@gmail.com>
2024-04-03 04:56:44 -0300

    :up: Project Flexbox: MediCenter Website 1.5 - Responsive

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# php-zero-professional
-PHP do Zero ao Profissional (Bonieky Lacerda)
+# > B7Web (Bonieky Lacerda)
+Curso: PHP do Zero ao Profissional
+
+# > Projetos
+## 1. Javascript
+- [Portal](https://github.com/riicard0/php-zero-professional/tree/main/projects/portal)
+- [MediCenter](https://github.com/riicard0/php-zero-professional/tree/main/projects/3.%20flexbox%20website)
+- 
--- projects/3. flexbox website/js/script.js
+++ projects/3. flexbox website/js/script.js
@@ -1,1 +1,10 @@
-"javascript"  
+//menu mobile
+window.onload = function() {
+  document.querySelector(".menuMobile").addEventListener("click", function(){
+    if(document.querySelector(".menu nav ul").style.display == "flex") {
+      document.querySelector(".menu nav ul").style.display = 'none';
+    } else {
+      document.querySelector(".menu nav ul").style.display = 'flex';
+    }
+  });
+}
