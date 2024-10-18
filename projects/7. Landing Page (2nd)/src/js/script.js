d48e6301b098cfd44775e2a210a5c23f1add92d0
Ricardo Martins <riicardo.marttyns@gmail.com>
2024-10-18 15:45:31 -0300

    :up: 2nd Landing Page Project 1.9 - Team

--- projects/7. Landing Page (2nd)/src/js/script.js
+++ projects/7. Landing Page (2nd)/src/js/script.js
@@ -121,3 +121,38 @@ function open_img(evt, cityName){
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.classList.add('active');
 }
+
+// Slider - Team Section
+$('.team-slider').slick({
+    arrows: false,
+    dots: true,
+    infinite: false,
+    autoplay: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive:[{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true,
+        }
+    },
+    {
+        breakpoint: 600,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+        }
+    },
+    {
+        breakpoint: 480,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+    }
+]
+});
