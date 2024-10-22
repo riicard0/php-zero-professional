cd4b5e975326275c6b4a7263966b87fe5d557ba6
Ricardo Martins <riicardo.marttyns@gmail.com>
2024-10-22 11:38:34 -0300

    :up: 2nd Landing Page Project 1.11 - FAQ

--- projects/7. Landing Page (2nd)/src/js/script.js
+++ projects/7. Landing Page (2nd)/src/js/script.js
@@ -156,3 +156,45 @@ $('.team-slider').slick({
     }
 ]
 });
+
+// Accordion FAQ
+document.addEventListener('DOMContentLoaded', function() {
+    let accordionButtons = document.querySelectorAll('.accordion-button');
+    let acoimg = document.querySelectorAll('.accordion-button img');
+
+    accordionButtons.forEach(function(button, index) {
+        button.addEventListener('click', function() {
+            let collapse = this.parentElement.nextElementSibling;
+
+            // close all the other accordion items
+            accordionButtons.forEach(function(otherButton, otherIndex){
+                if(otherButton !== button){
+                    let otherCollapse = otherButton.parentElement.nextElementSibling;
+                    otherCollapse.style.maxHeight = null;
+
+                    // reset the image source and rotation for the other accordion items
+                    acoimg[otherIndex].src = 'src/assets/images/icon/plus.png';
+                    acoimg[otherIndex].style.transform = "rotate(0deg)";
+                    otherButton.style.backgroundColor = "#fff";
+                }
+            });
+
+            // toggle the clicked according item
+            if(collapse.style.maxHeight) {
+                collapse.style.maxHeight = null;
+
+                // reset the image source, rotation, and background color when collapsing
+                acoimg[index].src = 'src/assets/images/icon/plus.png';
+                acoimg[index].style.transform = "rotate(90deg)";
+                button.style.backgroundColor = '';
+            } else {
+                collapse.style.maxHeight = collapse.scrollHeight + "px";
+
+                // change the image source, set rotation, and background color when expanding
+                acoimg[index].src = 'src/assets/images/icon/menus.png';
+                acoimg[index].style.transform = "rotate(180deg)";
+                button.style.backgroundColor = "#c1b0b5";
+            }
+        });
+    });
+});
